ec7d490bb91bdb02ad1930901400ed5349539057
mesametamaarkhan <mesamtamaark@gmail.com>
2025-04-18 20:01:57 +0500

    basic server setup

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_req, res) => {
+  res.send("Hello from TypeScript!");
+});
+
+export default app;
